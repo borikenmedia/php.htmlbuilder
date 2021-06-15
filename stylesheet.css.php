849f55b3ff5e670c6cfb50a84be9d43cc147c093
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-06-15 11:58:02 -0400

    Update stylesheet.css.php

--- stylesheet.css.php
+++ stylesheet.css.php
@@ -9,3 +9,93 @@ body{
 	font: normal 1em "Lucida Sans Unicode", sans-serif;
 	color: #000;
 }
+
+div#wrapper{
+	margin: 1em Auto;
+	padding: 1%;
+	width: 63%;
+	background: #f1f1f1;
+	border: solid .111em #aaa;
+	font: normal .75em "Lucida Sans Unicode", sans-serif;
+	color: #000;
+}
+
+div#banner{
+	margin: 0;
+	padding: 1%;
+	width: 98%;
+}
+
+div#tabmenu{}
+
+div#tabmenu ul li{
+	margin: 0;
+	padding: 0;
+	list-style: none;
+}
+
+div#tabmenu a:link, a:active, a:visited{
+	margin: 0 .111em;
+	padding: 1%;
+	width: 17.32%;
+	background: #fff;
+	border-width: .111em .111em .111em .5em;
+	border-style: solid;
+	border-color: #ccc;
+	font: bold 1em "Lucida Sans Unicode", sans-serif;
+	color: #000;
+	text-decoration: none;
+	float: left;
+	display: inline-table;
+}
+
+div#tabmenu a:hover{
+	margin: 0 .15em;
+	padding: 1%;
+	width: 17.20%;
+	background: #fff;
+	border-width: .111em .111em .111em .5em;
+	border-style: solid;
+	border-color: #ccc #ccc #ccc #666;
+	text-decoration: underline;
+	float: left;
+	display: inline-table;
+}
+
+div#sitemap{
+	margin: .15em 0;
+	padding: 1%;
+	width: 98%;
+	background: #fff;
+	border: solid .111em #ccc;
+}
+
+div#content{}
+
+div#sidebar{
+	margin: 0 .1em;
+	padding: 1%;
+	width: 27.65%;
+	background: #fff;
+	border: solid .111em #ccc;
+	float: left;
+	display: inline-table;
+}
+
+div#information{
+	margin: 0;
+	padding: 1%;
+	width: 68%;
+	background: #fff;
+	border: solid .111em #ccc;
+	float: left;
+	display: inline-table;
+}
+
+div#copyright{
+	margin: .15em 0;
+	padding: 1%;
+	width: 98%;
+	background: #fff;
+	border: solid .111em #ccc;
+}
