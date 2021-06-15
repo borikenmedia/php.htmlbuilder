f7ee7a469c39d721bc400d2851f6d13911d88447
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-06-15 11:58:51 -0400

    Update template.html.php

--- template.html.php
+++ template.html.php
@@ -1,20 +1,45 @@
 <!DOCTYPE html Public>
 <html lang="en_us">
 <head>
-<title>{title}</title>
+<title>{Title}</title>
 <meta name="description" content="Web Design And Development" />
 <meta name="keywords" content="Web, Design, Development, Ttorials" />
 <meta name="aucthor" content="dyewilliam" />
 <meta charset=utf-8 />
 <style type="text/css">
 <!--
-/{Style}/
+{Style}
 //-->
 </style>
 </head>
 <body>
 
-{COntent}
-
+<div id="wrapper">
+	<div id="banner">
+		<h2>Boriken Media Subs</h2>
+	</div>
+	<div id="tabmenu">
+		<ul>
+			<li><a href="#" target="_self" title="LINK">HOME</a></li>
+			<li><a href="#" target="_self" title="LINK">ABOUT</a></li>
+			<li><a href="#" target="_self" title="LINK">SERVICE</a></li>
+			<li><a href="#" target="_self" title="LINK">POSTS</a></li>
+			<li><a href="#" target="_self" title="LINK">CONTACT</a></li>a
+		</ul>
+	</div>
+	<div style="clear:both;"></div>
+	<div id="sitemap">
+		Home &gt; {Sitemap}
+	</div>
+	<div id="content">
+		<div id="sidebar">{Sidebar}</div>
+		<div id="information">{Data}</div>
+	</div>
+	<div style="clear:both;"></div>
+	<div id="copyright">
+		{Content}
+	</div>
+</div>
+	
 </body>
 </html>
